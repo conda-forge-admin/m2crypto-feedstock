e6c9efa24113e95d4b1c481effb8e089478a73d1
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-06-01 00:20:21 -0500

    Support windows

--- recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-# Use PREFIX to use conda openssl
-"%PYTHON%" setup.py build_ext --openssl %LIBRARY_PREFIX%
-if errorlevel 1 exit 1
-
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
